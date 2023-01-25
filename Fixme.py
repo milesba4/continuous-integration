5aa08e7d02cbda4c75a4c2a1a57adbdd872cde0e
Miles Baffour <mbaffour24@cmc.edu>
2023-01-24 22:49:42 -0800

    minor change in order to see actions builds

--- Fixme.py
+++ Fixme.py
@@ -2,7 +2,7 @@
 
 def factorial(n):
     '''
-    Returns the product of all numbers from 1 to n.
+    Return the product of all numbers from 1 to n.
     >>> factorial(1)
     1
     >>> factorial(2)
